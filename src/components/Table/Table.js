3a7e8e63c581459130f6dbb36899a1b426865d75
Macbook <macbook@MacBook-Pro.local>
2023-02-23 02:41:07 -0800

    changes

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,9 +1,12 @@
+import { UsergroupAddOutlined } from '@ant-design/icons';
 import { Form, Input, InputNumber, Popconfirm, Table, Typography } from 'antd';
 import { useState } from 'react';
  
 const onChange = (pagination, filters, sorter, extra) => {
-  console.log('params', sorter);
+  console.log('params', sorter,filters, "COming");
 };
+
+
 const EditableCell = ({
   editing,
   dataIndex,
@@ -40,22 +43,45 @@ const EditableCell = ({
 };
 const CustomTable = () => {
   const [form] = Form.useForm();
-  const [originData, setOriginDate] = useState([])
+  const [originData, setOriginData] = useState( [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sydney No. 1 Lake Park',
+    },
+    {
+      key: '4',
+      name: 'Jim Red',
+      age: 32,
+      address: 'London No. 2 Lake Park',
+    },
+  ])
   const [data, setData] = useState(originData);
   const [editingKey, setEditingKey] = useState('');
   const isEditing = (record) => record.key === editingKey;
-  for (let i = 0; i < 100; i++) {
-    originData.push({
-      key: i.toString(),
-      name: `Edrward ${i}`,
-      age: 32,
-      address: `London Park no. ${i}`,
-    });
-  }
   const handleDelete = (key) => {
     const newData = originData.filter((item) => item.key !== key);
-    setOriginDate(newData);
+    setOriginData(newData);
   };
+  
+  
+  function sortDataByGroup(key) {
+    const newData = [...originData].sort((a, b) => a.name.localeCompare(b.name));
+    setOriginData(newData);
+  }
   const edit = (record) => {
     form.setFieldsValue({
       name: '',
@@ -92,37 +118,44 @@ const CustomTable = () => {
   };
   const columns = [
     {
+      title: "NO",
+      dataIndex: "id",
+      key: "id",
+      width: "15%",
+      sorter: (a, b) => a.id - b.id,
+      render: (data, index, element) => {
+        return <p onClick={console.log(element)}>{element + 1}</p>
+      },
+    },
+    {
       title: 'name',
       dataIndex: 'name',
-      width: '25%',
-      sorter: (a, b) => a.name - b.name,
+      width: '30%',
+      // sorter: (a, b) => a.name - b.name,
+     
+      
             editable: true,
     },
     {
       title: 'age',
       dataIndex: 'age',
-      width: '15%',
+      width: '10%',
       sorter: (a, b) => a.age - b.age,
       editable: true,
     },
+    
     {
       title: 'address',
       dataIndex: 'address',
-      sorter: (a, b) => a.address - b.address,
+      // sorter: (a, b) => a.address - b.address,
+      onHeaderCell: (column) => {
+        <></>
+      },
 
       width: '40%',
       editable: true,
     },
-    {
-      title: 'operation',
-      dataIndex: 'operation',
-      render: (_, record) =>
-      originData.length >= 1 ? (
-          <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.key) }>
-            <a>Delete</a>
-          </Popconfirm>
-        ) : null,
-    },
+    
     {
       title: 'operation',
       dataIndex: 'operation',
@@ -148,6 +181,16 @@ const CustomTable = () => {
           </Typography.Link>
         );
       },
+    },
+    {
+      title: 'operation',
+      dataIndex: 'operation',
+      render: (_, record) =>
+      originData.length >= 1 ? (
+          <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.key) }>
+            <a>Delete</a>
+          </Popconfirm>
+        ) : null,
     },
   ];
   const mergedColumns = columns.map((col) => {
