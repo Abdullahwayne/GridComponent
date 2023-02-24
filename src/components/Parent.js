d0a908a0d13e972e6a791e4e80fa930d40c04a1d
Muhammad Bilal Aleem <30667628+brad911@users.noreply.github.com>
2023-02-24 12:01:55 +0500

    added tabination in parent

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,13 +1,55 @@
 import { Table } from "antd";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import CustomTable from "./Table/Table";
-import sampleData from "../Assets/data.json";
+import sampleData from "../Assets/SampleData.json";
+import { Tabs } from "antd";
+
 const Parent = () => {
+  const items = [
+    {
+      key: "1",
+      label: `Tab 1`,
+      children: `wow`,
+    },
+    {
+      key: "2",
+      label: `Tab 2`,
+      children: `Content of Tab Pane 2`,
+    },
+    {
+      key: "3",
+      label: `Tab 3`,
+      children: `Content of Tab Pane 3`,
+    },
+  ];
+  const [data, setData] = useState([]);
+  const setTabs = () => {
+    console.log("i ran");
+    let newData = [];
+    for (let index = 0; index < Object.keys(sampleData).length; index++) {
+      const element = Object.keys(sampleData)[index];
+      console.log(element, "<===");
+      console.log(sampleData[element], "<=== data");
+      newData.push({
+        key: (index + 1).toString(),
+        label: element,
+        children: <CustomTable dataSet={sampleData[element]} title={element} />,
+      });
+    }
+    console.log(newData, ",=== new data");
+    setData(newData);
+  };
+  const onChange = (key) => {
+    console.log(key);
+  };
+
+  useEffect(() => {
+    setTabs();
+  }, []);
+
   return (
     <div>
-      {Object.keys(sampleData).map((item, index) => {
-        return <CustomTable dataSet={sampleData[item]} title={item} />;
-      })}
+      <Tabs defaultActiveKey="1" onChange={onChange} items={data} />
     </div>
   );
 };
